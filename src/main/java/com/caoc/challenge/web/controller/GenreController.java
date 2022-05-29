9fe3c1d0b73061bc44a031acc718df94aeb6df8e
john.pineros <CO1015405675@enel.com>
2022-05-28 19:32:50 -0500

    Controller gender

--- src/main/java/com/caoc/challenge/web/controller/GenreController.java
+++ src/main/java/com/caoc/challenge/web/controller/GenreController.java
@@ -1,9 +1,43 @@
 package com.caoc.challenge.web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.caoc.challenge.domain.services.GenreService;
+import com.caoc.challenge.web.dto.GenreDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/genre")
 public class GenreController {
+    @Autowired
+    private GenreService genreService;
+
+    @GetMapping("")
+    public ResponseEntity<List<GenreDTO>> getAll() {
+        return new ResponseEntity<>(genreService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenreDTO> getById(@PathVariable Long id) {
+        return new ResponseEntity<>(genreService.getById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<GenreDTO> create(@RequestBody GenreDTO genreDTO) {
+        return new ResponseEntity<>(genreService.create(genreDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<GenreDTO> update(@RequestBody GenreDTO genreDTO) {
+        return new ResponseEntity<>(genreService.update(genreDTO), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+        genreService.delete(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
