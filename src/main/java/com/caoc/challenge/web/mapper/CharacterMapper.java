d2dbd9e8ca45064e491490162f9af712229b6af6
caobandoc <obandocarlosandres@gmail.com>
2022-05-29 22:10:13 -0500

    Correccion CharacterMapper

--- src/main/java/com/caoc/challenge/web/mapper/CharacterMapper.java
+++ src/main/java/com/caoc/challenge/web/mapper/CharacterMapper.java
@@ -1,13 +1,15 @@
 package com.caoc.challenge.web.mapper;
 
+import com.caoc.challenge.domain.entity.Movie;
 import com.caoc.challenge.web.dto.CharacterDTO;
 import com.caoc.challenge.domain.entity.Character;
 import com.caoc.challenge.web.dto.CharacterParamDTO;
+import com.caoc.challenge.web.dto.MovieDTO;
 import org.mapstruct.*;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = {MovieMapper.class})
+@Mapper(componentModel = "spring")
 public interface CharacterMapper {
     //Entity - DTO
     @Mappings({
@@ -33,4 +35,18 @@ public interface CharacterMapper {
     CharacterParamDTO characterToCharacterParamDTO(Character character);
     List<CharacterParamDTO> characterListToCharacterParamDTO(List<Character> characterList);
 
+    @Mappings({
+            @Mapping(source="id", target="id"),
+            @Mapping(source="image", target="imagen"),
+            @Mapping(source="title", target="titulo"),
+            @Mapping(source="creationDate", target="fechaCreacion"),
+            @Mapping(source="rate", target="calificacion")
+
+    })
+    MovieDTO movieToMovieDTO(Movie movie);
+    List<MovieDTO> movieListToMovieDTOList(List<Movie> movieList);
+
+    @InheritInverseConfiguration
+    Movie movieDTOToMovie(MovieDTO movieDTO);
+    List<Movie> movieDTOListToMovieList(List<MovieDTO> movieDTOList);
 }
